ee563004ab00fa9dfa6c3ccf6ef4c9e7d88ee219
Sichu <zhangsichu@gmail.com>
2015-08-04 16:02:40 +0800

    all the page and initial function code.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,12 +3,36 @@
  */
 angular.module('ddApp.controllers', ['ddApp.services'])
     .controller('LoginCtrl', function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+       $scope.doLogin = function() {
+           $state.go('list', {}, {reload: true});
+       }
     })
     .controller('ListCtrl', function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+        $scope.goDetail = function() {
+            $state.go('detail', {}, {reload: true});
+        };
+        $scope.goScan = function() {
+            $state.go('scan');
+        };
+        $scope.goManual = function() {
+            $state.go('manual');
+        };
+        $scope.doExit = function(){
+            $state.go('login');
+        }
     })
     .controller('DetailCtrl', function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+        $scope.goList = function() {
+            $state.go('list', {}, {reload: true});
+        }
     })
-    .controller('MenuCtrl', function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+    .controller('ManualCtrl', function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+        $scope.goDetail = function() {
+            $state.go('detail', {}, {reload: true});
+        };
     })
     .controller('ScanCtrl', function ($scope, $state, $ionicPopup, $ionicHistory, $ionicViewSwitcher, $ionicLoading) {
+        $scope.goDetail = function() {
+            $state.go('detail', {}, {reload: true});
+        };
     })
